14cfaafad20b9128b84c469370f9d6d1fc32ed35
Amandine Nassiri <amandine.nassiri@epita.fr>
2020-11-11 02:17:58 +0100

    server: initial commit

---
+++ server/server.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char **)
+{
+    if (argc != 3)
+    {
+        std::cerr << "Usage: ./server IP PORT\n";
+        return 1;
+    }
+
+    // create server socket
+    // server loop
+    // close socket
+
+    return 0;
+}
