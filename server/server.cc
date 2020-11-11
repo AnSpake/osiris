0aaaa5b2d431535362ee3b2f39c47357b49120c8
Amandine Nassiri <amandine.nassiri@epita.fr>
2020-11-11 02:58:50 +0100

    server: set up getaddrinfo

--- server/server.cc
+++ server/server.cc
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <cstring>
 
-int main(int argc, char **)
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int prepare_socket(const std::string& ip, const std::string& port)
+{
+    struct addrinfo *result;
+
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    if (getaddrinfo(ip.c_str(), port.c_str(), &hints, &result))
+    {
+        std::cerr << "getaddinfo failed !\n";
+        exit(EXIT_FAILURE);
+    }
+
+    int socket = create_and_bind();
+    listen(socket, SOMAXCONN);
+    return socket;
+}
+
+int main(int argc, char **argv)
 {
     if (argc != 3)
     {
@@ -8,7 +35,7 @@ int main(int argc, char **)
         return 1;
     }
 
-    // create server socket
+    int server_socket = prepare_socket(argv[1], argv[2]);
     // server loop
     // close socket
 
